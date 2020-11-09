9fcb68fa347ef75eeba1516f6fdaf1e40284c187
KleberNeves <kleber.na@gmail.com>
2020-11-09 14:25:50 -0300

    Add function to extract full affiliation (makes extract author country order obsolete)

--- R/biblio.R
+++ R/biblio.R
@@ -179,3 +179,109 @@ extract_countries = function (M, count_unique = F) {
     countries
   })
 }
+
+#' Extracts affiliation of individual authors
+#'
+#' From a M bibliometrix data frame, extracts the author affiliation and country
+#' from the AU and C1 fields. Lots of ad hoc work arounds to handle abbreviated names and such.
+#' Author order comes in two columns, as counted from first to last (positive) and from last to first (negative, i.e. -1 is the last author).
+#'
+#' @param M A bibliometrix dataset.
+#' @return A data frame with the author order and country data.
+#' @export
+extract_author_affiliations = function (M) {
+  author_country_data = plyr::ldply(1:nrow(M), function (i) {
+    title = M[i, "TI"]
+    affil = M[i, "C1"]
+    author_field = M[i, "AU"]
+
+    if (is.na(affil) | !stringr::str_detect(affil, "\\[")) {
+      return (
+        data.frame(Title = title,
+                   Author = NA,
+                   Position = NA,
+                   Country = NA,
+                   stringsAsFactors = F)
+      )
+    }
+
+    # Extract author list and positions to be merged later
+    author_list = unlist(stringr::str_split(author_field, ";"))
+    author_list = data.frame(Author = author_list, Position = 1:length(author_list))
+    author_list$NegPosition = author_list$Position - nrow(author_list) - 1
+
+    # Extracts author list from affiliation text
+    authors = unlist(stringr::str_extract_all(affil, "(\\[.+?\\])"))
+    authors = stringr::str_replace_all(authors, "[.];", ".")
+    nauthors_per_group = stringr::str_count(authors, ";") + 1
+    authors = stringr::str_remove_all(authors, "\\[")
+    authors = stringr::str_remove_all(authors, "\\]")
+    authors = stringr::str_remove_all(authors, ",")
+    authors = stringr::str_remove_all(authors, "[.]")
+    authors = unlist(stringr::str_split(authors, "; "))
+
+    # Extracts affiliations per group of authors
+    affil = stringr::str_replace_all(affil, "; \\[", " [")
+    affil = stringr::str_replace_all(affil, "(\\[.+?\\])", "]")
+    affil = unlist(stringr::str_split(affil, "\\] "))
+    affil = affil[2:length(affil)]
+    affil = affil[affil != ""]
+
+    affils = rep(affil, nauthors_per_group) %>%
+      str_remove_all("(;$)|([.] $)")
+
+    # Extract countries from affiliations
+    countries = unlist(plyr::llply(affil, function (x) {
+      x = unlist(stringr::str_split(x, "; "))
+      x = stringr::str_trim(x)
+      x = stringr::str_remove_all(x, "[.]")
+      x = stringr::str_remove(x, ".+, ")
+      x[stringr::str_which(x, " USA$")] = "USA"
+      x = paste(x, collapse = "; ")
+    }))
+    countries = rep(countries, nauthors_per_group)
+
+    # Building results data frame
+    R = data.frame(Title = title,
+                   Author = authors,
+                   Affiliation = affils,
+                   Country = countries,
+                   stringsAsFactors = F)
+
+    RR = merge(R, author_list, by = "Author")
+
+    # If merge fails, see if author list is using initials and remerge
+    if (nrow(RR) == 0) {
+      author_names = unlist(lapply(authors, function (x) {
+        x = unlist(stringr::str_split(x, " "))
+        first_name = x[1]
+        if (first_name %in% c("DE","DOS","DAS","DA")) {
+          first_name = paste(x[1:2], collapse = " ")
+          x = x[3:length(x)]
+        } else {
+          x = x[2:length(x)]
+        }
+
+        if (any(x %in% c("DE","DOS","DAS","DA"))) {
+          dosdas = which(x %in% c("DE","DOS","DAS","DA")) + 1
+          x = x[-dosdas]
+        }
+        x = stringr::str_extract(x, "[A-Z]")
+        # } else { x = c() }
+        fullname = paste(c(first_name, " ", x), collapse = "")
+        fullname
+      }))
+
+      R = data.frame(Title = title,
+                     Author = author_names,
+                     Affiliation = affils,
+                     Country = countries,
+                     stringsAsFactors = F)
+      RR = merge(R, author_list, by = "Author")
+    }
+
+    RR
+  })
+
+  author_country_data
+}
